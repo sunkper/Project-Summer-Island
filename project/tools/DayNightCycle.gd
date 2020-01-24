324ddd2f3866f57b3da829ad572d3c7fd44488df
sunkper <4294611+sunkper@users.noreply.github.com>
2020-01-24 15:13:29 +0900

    Add rudimentary day/night cycle; adjust handrail

---
+++ project/tools/DayNightCycle.gd
@@ -0,0 +1,29 @@
+tool
+extends Node
+
+# Current implementation: just switching between day and night
+
+enum times {DAY, NIGHT}
+export (times) var time setget _set_time
+
+export (NodePath) var environment
+export (NodePath) var dir_light
+
+onready var panorama_day = preload("res://_TEMP_THEFT_ASSETS/textures/Boku2SkyDay.jpg")
+onready var panorama_night = preload("res://_TEMP_THEFT_ASSETS/textures/Boku2SkyNight.jpg")
+
+func _set_time(new_time) -> void:
+	time = new_time
+	if not get_parent().get_child_count() > 0:
+		return
+	var env = get_node(environment).environment
+	
+	match time:
+		times.DAY:
+			env.background_sky.panorama = panorama_day
+			env.background_energy = 0.2
+			get_node(dir_light).light_color = Color(0.98, 0.97, 0.93)
+		times.NIGHT:
+			env.background_sky.panorama = panorama_night
+			env.background_energy = 0.2
+			get_node(dir_light).light_color = Color(0.08, 0.12, 0.24)
