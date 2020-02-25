e02e6d5bed9c51ff424fff85b33540b88a313721
sunkper <4294611+sunkper@users.noreply.github.com>
2020-02-25 13:48:21 +0900

    LineUpMeshInstance now accepts rotation

--- project/tools/LineUpMeshInstance.gd
+++ project/tools/LineUpMeshInstance.gd
@@ -9,7 +9,10 @@ export var grid := Vector3(2, 2, 2) setget _set_grid
 export var grid_random := Vector3(0, 0, 0) setget _set_grid_random
 
 export var offset := Vector3(0.5, 0.5, 0.5) setget _set_offset
-export var offset_random: = Vector3(0, 0, 0) setget _set_offset_random
+export var offset_random:= Vector3(0, 0, 0) setget _set_offset_random
+
+export var rot := Vector3(0.0, 0.0, 0.0) setget _set_rot
+export var rot_random:= Vector3(0.0, 0.0, 0.0) setget _set_rot_random
 
 
 func _set_mesh(new_mesh) -> void:
@@ -69,6 +72,20 @@ func _set_offset_random(new_offset_random: Vector3) -> void:
 	offset_random = new_offset_random
 	update()
 
+func _set_rot(new_rot: Vector3) -> void:
+	if new_rot == rot:
+		return
+	
+	rot = new_rot
+	update()
+
+func _set_rot_random(new_rot_random: Vector3) -> void:
+	if new_rot_random == rot_random:
+		return
+	
+	rot_random = new_rot_random
+	update()
+
 func _create_multimesh() -> void:
 	multimesh = MultiMesh.new()
 	multimesh.transform_format = MultiMesh.TRANSFORM_3D
@@ -90,17 +107,39 @@ func update() -> void:
 	for x in range(int(final_grid.x)):
 		for y in range(int(final_grid.y)):
 			for z in range(int(final_grid.z)):
-				var final_offset = _get_offset(offset, offset_random)
-				var xform = Transform(Vector3(1, 0, 0), Vector3(0, 1, 0), Vector3(0, 0, 1), Vector3(x * final_offset.x, y * final_offset.y, z * final_offset.z))
+				var final_pos = _get_final_pos(offset, offset_random)
+				var final_basis = _get_basis(rot, rot_random)
+				var xform = Transform(final_basis, Vector3(x * final_pos.x, y * final_pos.y, z * final_pos.z))
 				multimesh.set_instance_transform(i, xform)
 				i += 1
 
-func _get_offset(base: Vector3, random_range: Vector3) -> Vector3:
-	if random_range.x != 0.0:
-		base.x = base.x + rand_range(0.0, random_range.x)
-	if random_range.y != 0.0:
-		base.y = base.y + rand_range(0.0, random_range.y)
-	if random_range.z != 0.0:
-		base.z = base.z + rand_range(0.0, random_range.z)
+func _get_final_pos(base: Vector3, rand: Vector3) -> Vector3:
+	if rand.x != 0.0:
+		base.x = base.x + rand_range(0.0, rand.x)
+	if rand.y != 0.0:
+		base.y = base.y + rand_range(0.0, rand.y)
+	if rand.z != 0.0:
+		base.z = base.z + rand_range(0.0, rand.z)
 	
 	return base
+
+func _get_basis(base: Vector3, rand: Vector3) -> Basis:
+	var basis := Basis()
+	if base == Vector3() and rand == Vector3():
+		return basis
+	
+	if rand.x != 0.0:
+		base.x = base.x + rand_range(0.0, rand.x)
+	if rand.y != 0.0:
+		base.y = base.y + rand_range(0.0, rand.y)
+	if rand.z != 0.0:
+		base.z = base.z + rand_range(0.0, rand.z)
+	
+	# x rotation
+	basis = basis.rotated(Vector3(1, 0, 0), deg2rad(base.x))
+	# y rotation
+	basis = basis.rotated(Vector3(0, 1, 0), deg2rad(base.y))
+	# z rotation
+	basis = basis.rotated(Vector3(0, 0, 1), deg2rad(base.z))
+	
+	return basis
