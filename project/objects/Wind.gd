6d1911eba0b8d100207c023c71fd30af971673c8
sunkper <4294611+sunkper@users.noreply.github.com>
2020-03-18 14:01:44 +0900

    Add cloth tied to pole, make each wind to have own shape

--- project/objects/Wind.gd
+++ project/objects/Wind.gd
@@ -1,8 +1,8 @@
 extends KinematicBody
 
 export var dir: = Vector3(0.0, 0.0, 1.0) setget _changed_dir
-export (float, 0.001, 10.0) var speed: = 1.0 setget _changed_speed
-export (float, 0.001, 20.0) var length: = 1.0 setget _changed_length
+export (float, 0.001, 10.0) var speed: = 1.0
+export (float, 0.001, 20.0) var length: = 1.0
 export (float, 0.001, 10.0) var radius: = 0.5 setget _changed_radius
 
 export var blow: = false setget _start_blow
@@ -15,24 +15,13 @@ func _changed_dir(new_dir) -> void:
 	
 	dir = new_dir.normalized()
 
-func _changed_speed(new_speed) -> void:
-	if new_speed == speed:
-		return
-	
-	speed = new_speed
-
-func _changed_length(new_length) -> void:
-	if new_length == length:
-		return
-	
-	length = new_length
-
 func _changed_radius(new_radius) -> void:
 	if new_radius == radius:
 		return
 	
 	radius = new_radius
-	$CollisionShape.shape.radius = radius
+	if $CollisionShape.shape != null:
+		$CollisionShape.shape.radius = radius
 
 func _start_blow(new_state) -> void:
 	if new_state == blow:
@@ -48,6 +37,9 @@ func _start_blow(new_state) -> void:
 
 func _ready() -> void:
 	orig_pos = translation
+	var c = SphereShape.new()
+	$CollisionShape.shape = c
+	$CollisionShape.shape.radius = radius
 
 func _physics_process(delta: float) -> void:
 	translation += dir * speed * delta
