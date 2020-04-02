00fb1c371d66e5bf80ec17d04df8034e7f0a79ca
sunkper <4294611+sunkper@users.noreply.github.com>
2020-04-02 16:45:17 +0900

    Polish player controller, now you are a ghost? Drone?

--- README.md
+++ README.md
@@ -9,7 +9,9 @@ This project is made with Godot Engine 3.2.1.
 Plugins used in this project are not included in the repositary. You should install them yourself. Versions may or may not be relavant for compatibility, look for version history of each plugin.
 
 * [Qodot](https://github.com/ShiftyAxel/Qodot) (1.6.4)
-* [Godot God Rays Plugin](1.0.1)
+* [Godot God Rays Plugin](https://github.com/SIsilicon/Godot-God-Rays-Plugin) (1.0.1)
+* [Godot Heightmap Terrain](https://github.com/Zylann/godot_heightmap_plugin) (1.1)
+* [Smoothing](https://github.com/lawnjelly/smoothing-addon) (1.0.0)
 
 ## License
 My original creations are licensed under MIT License. **HOWEVER** as this is a re-creation of a scene from a commerical game, there are important caveats. Please refer to [License file](https://github.com/sunkper/Project-Summer-Island/blob/master/LICENSE.md).
--- project/actors/Player.gd
+++ project/actors/Player.gd
@@ -1,36 +1,60 @@
 extends KinematicBody
 
-export var speed_def_max: = 7.0
-export var speed_sprint_max = 15.0
+enum State {IDLE, MOVING}
+var state = State.IDLE setget _set_state
 
-const GRAVITY = 10.0
+export var speed_def_max: = 7.0
+export var speed_sprint_max: = 15.0
+
+export var mov_lerp_weight: = 0.1
 
 var move_speed: = 0.0
 var is_sprinting: = false
-var is_flying: = true
 
 var vel: = Vector3.ZERO
-
 var dir: = Vector3.ZERO
 
+var float_effect: = true
+var elpased: = 0.0
+
 export var mouse_def_sensi: = 0.2
-export var mouse_zoomin_sensi: = 0.03
+export var mouse_zoomin_sensi: = 0.06
 var mouse_sensi: = mouse_def_sensi
 
 export var def_fov: = 75.0
 export var zoom_fov: = 30.0
 
 var zooming: = false
-var zoomin_lerp: = 0.05
-var zoomout_lerp: = 0.15
+var zoomin_lerp: = 0.10
+var zoomout_lerp: = 0.30
 
+# simple state management
+func _set_state(new_state) -> void:
+	if new_state == state:
+		return
+	
+	# exit behavior
+	match state:
+		State.IDLE:
+			pass
+		State.MOVING:
+			pass
+	
+	state = new_state
+	
+	# init behavior
+	match state:
+		State.IDLE:
+			pass
+		State.MOVING:
+			pass
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _physics_process(delta: float) -> void:
 	dir = Vector3.ZERO
-	var cam_xform = $Camera.get_camera_transform()
+	var cam_xform = $Smoothing/Camera.get_camera_transform()
 	
 	var move_vector = _get_move_input()
 	
@@ -41,12 +65,27 @@ func _physics_process(delta: float) -> void:
 		dir += cam_xform.basis.x * move_vector.x
 		dir += cam_xform.basis.y * move_vector.z
 	
-	vel = dir * move_speed
-	
-	if not is_flying:
-		vel.y -= GRAVITY
+	vel = lerp(vel, dir * move_speed, 0.1)
+	if vel.length() > 0.0:
+		self.state == State.MOVING
+	else:
+		self.state == State.IDLE
 	
 	vel = move_and_slide(vel, Vector3(0, 1, 0), true)
+	
+	elpased += delta
+	if float_effect:
+		move_and_collide(Vector3(0, sin(elpased * 2.5) * 0.004, 0))
+	
+	# crude implementation of zoom-in
+	if zooming:
+		$Smoothing/Camera.fov = lerp($Smoothing/Camera.fov, zoom_fov, zoomin_lerp)
+		mouse_sensi = lerp(mouse_sensi, mouse_zoomin_sensi, zoomin_lerp)
+		$UI/Vignette.modulate.a = lerp($UI/Vignette.modulate.a, 1.0, zoomin_lerp)
+	else:
+		$Smoothing/Camera.fov = lerp($Smoothing/Camera.fov, def_fov, zoomout_lerp)
+		mouse_sensi = lerp(mouse_sensi, mouse_def_sensi, zoomout_lerp)
+		$UI/Vignette.modulate.a = lerp($UI/Vignette.modulate.a, 0.0, zoomout_lerp)
 
 func _get_move_input():
 	var out: = Vector3.ZERO
@@ -59,22 +98,22 @@ func _get_move_input():
 		out.x = -1
 	elif Input.is_action_pressed("strafe_right"):
 		out.x = 1
-	if is_flying:
-		if Input.is_action_pressed("ascend"):
-			out.z = 1
-		elif Input.is_action_pressed("descend"):
-			out.z = -1
+	
+	if Input.is_action_pressed("ascend"):
+		out.z = 1
+	elif Input.is_action_pressed("descend"):
+		out.z = -1
 	
 	return out
 
 func _input(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		$Camera.rotate_x(deg2rad(event.relative.y * mouse_sensi * -1))
+		$Smoothing/Camera.rotate_x(deg2rad(event.relative.y * mouse_sensi * -1))
 		rotate_y(deg2rad(event.relative.x * mouse_sensi * -1))
 		
-		var camera_rot = $Camera.rotation_degrees
+		var camera_rot = $Smoothing/Camera.rotation_degrees
 		camera_rot.x = clamp(camera_rot.x, -70, 70)
-		$Camera.rotation_degrees = camera_rot
+		$Smoothing/Camera.rotation_degrees = camera_rot
 	
 	if event.is_action_pressed("sprint"):
 		is_sprinting = true
@@ -82,7 +121,7 @@ func _input(event):
 		is_sprinting = false
 	
 	if event.is_action_pressed("flashlight"):
-		$Camera/Flashlight.visible = !$Camera/Flashlight.visible
+		$Smoothing/Camera/Flashlight.visible = !$Smoothing/Camera/Flashlight.visible
 	
 	if event.is_action_pressed("capture_mouse"):
 		if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
@@ -90,6 +129,10 @@ func _input(event):
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
+	if event.is_action_pressed("zoom_view"):
+		zooming = true
+	elif event.is_action_released("zoom_view"):
+		zooming = false
+	
 	if event.is_action_pressed("noclip"):
 		$BodyCollision.disabled = !$BodyCollision.disabled
-		$FeetCollision.disabled = !$FeetCollision.disabled
